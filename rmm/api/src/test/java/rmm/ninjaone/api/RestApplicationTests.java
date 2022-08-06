132b6202e4369f0b845ea943b7a3f42ec96adfd8
Arturo Menchaca <menchaca8808@gmail.com>
2022-08-07 01:18:27 +0200

    Added root gradle and api package.

---
+++ rmm/api/src/test/java/rmm/ninjaone/api/RestApplicationTests.java
@@ -0,0 +1,12 @@
+package rmm.ninjaone.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
+
